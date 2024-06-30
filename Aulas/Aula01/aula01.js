14a8062ddd48b09da8c7b35becdadfa321efb7a0
JenifferSR <jesouza774@gmail.com>
2024-06-29 22:56:18 -0300

    aula01

---
+++ Aulas/Aula01/aula01.js
@@ -0,0 +1,44 @@
+
+
+let numero = 10;
+let texto = "Olá mundo";
+let booleano = true;
+let nulo = null;
+let indefinido;
+let objeto = {
+    nome: "jeniffer",
+    idade: 25,
+    profissao : "Auxiliar de Exames"
+};
+let array = [1, 2, 3, 4, 5];
+
+var variavelGlobal = "eu sou uma variável global";
+let variavelLocal = "Eu sou uma variável local";
+const semAlterar = 2.15;
+
+
+console.log("Número:", numero);
+console.log("Texto:", texto);
+console.log("Booleano:", booleano);
+console.log("Nulo:", nulo);
+console.log("Indefinido:", indefinido);
+console.log("Objeto:", objeto);
+console.log("Array:", array);
+
+
+console.log("Variável Global", variavelGlobal);
+console.log("Variável Local", variavelLocal);
+console.log("Variável que o valor não pode alterar:", semAlterar);
+
+
+console.log("");
+console.log("Tabela de Exemplos:");
+console.log("| Tipo de Dado  |  Exemplo      | Descrição                  |");
+console.log("|---------------|---------------|----------------------------|");
+console.log("| Número        |", numero, "   | Número inteiro             |");
+console.log("| String        |", texto,"     | Sequência de caracteres    |");
+console.log("| Booleano      |", booleano,"  | Valor lógico verdadeiro    |");
+console.log("| Nulo:         |", nulo,"      | Valor nulo                 |");
+console.log("|Indefinido:    |", indefinido,"| Valor indefinido           |");
+console.log("|Objeto:        |", objeto,"    | Estrutura de dados complexa|");
+console.log("|Array:         |", array,"     | Coleção ordenada de valores|");
---
+++ Aulas/Aula01/variaveis.js
@@ -0,0 +1,10 @@
+
+let listaPessoas = ["Jeniffer", "Fabio", "Raul"]
+
+let nome = 'Jeniffer'
+let idade = 25
+let estaAprovado = false
+
+console.log(nome, idade, estaAprovado)
+
+console.log(listaPessoas)
